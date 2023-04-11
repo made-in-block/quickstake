cd8d6208d23c448eb216a4e6f06c00ca36937df2
dimiandre <dimiandre@gmail.com>
2023-04-11 17:08:32 +0200

    typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/favicon.png",
+        icon: "src/images/icon.png",
       },
     },
     "gatsby-plugin-sharp",
